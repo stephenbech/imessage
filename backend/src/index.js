e7ef662c85c780962e16533c9fe618bf829a2ba1
stephenbech <stephenbech19@gmail.com>
2026-07-25 05:55:33 +0100

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import "dotenv/config";
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+console.log(process.env.DB_URL);
+
+app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+});
