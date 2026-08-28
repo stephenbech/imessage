f9f19ef1490fc1366aebc5c9f7b662163e8b34f0
stephenbech <stephenbech19@gmail.com>
2026-08-28 12:38:43 +0100

    chore: import cron job module in index.js for scheduled tasks

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import path from "path";
 
 import { clerkMiddleware } from "@clerk/express";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
