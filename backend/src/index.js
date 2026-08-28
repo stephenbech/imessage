af9d2ccffd934dae726919140b06596ef94dc949
stephenbech <stephenbech19@gmail.com>
2026-08-28 13:43:31 +0100

    feat: implement Clerk webhook for user management and verification

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,23 @@
+import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import fs from "fs";
 import path from "path";
-
-import { clerkMiddleware } from "@clerk/express";
-import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
+import { connectDB } from "./lib/db.js";
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
+
+app.use(
+      "/api/webhooks/clerk",
+      express.raw({ type: "application/json" }),
+      clerkWebhook,
+);
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
